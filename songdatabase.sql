22ff8ffab1908b8ad4c7756bc99161cfad00ca3f
piyusharya8700 <piyusharya3003@proton.me>
2026-06-25 22:44:08 +0530

    Update songdatabase.sql

--- songdatabase.sql
+++ songdatabase.sql
@@ -1,1 +1,20 @@
+-- Q1 senior most employee based on job title
 
+select * from employee
+select * from employee order by levels desc limit 1
+
+
+-- Q2 which countries have most invoices?
+
+select * from invoice
+select count(*) as count_of_invoices, billing_country from invoice group by billing_country order by count_of_invoices desc
+
+-- Q3 what are the top 3 values of total invoice
+
+select * from invoice order by total desc limit 3
+
+
+--Q4 which city has the best customers? We would like to throw a promotional music festival in the city we made the most money
+--Write a query that returns one city that has the highest sum of invoice totals. Return both city name & sum of all invoice totals
+
+select billing_city, sum(total) as total_money from invoice group by billing_city order by total_money desc limit 1
